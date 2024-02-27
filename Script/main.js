6c9b164e8b9088c73f41bebd1240b9f2e5c74e36
Mickael Allouche <allouchemicka@gmail.com>
2024-02-27 17:12:54 +0200

    Add input validation for email and phone number

--- Script/main.js
+++ Script/main.js
@@ -7,3 +7,23 @@ document.getElementById(`exitNav`).addEventListener(`click`, () => {
     miniNav.style.display = `none`;
 });
 
+const theEmail = document.getElementById(`contactEmail`);
+const thePhone = document.getElementById(`contactPhone`);
+const regEx = /^(05|08)[0-9]{8}$/;
+const regExEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+thePhone.addEventListener(`input`, () => {
+    if (!regEx.test(thePhone.value)) {
+        thePhone.setCustomValidity(`Invalid phone number`);
+    } else {
+        thePhone.setCustomValidity(``);
+    }
+});
+
+theEmail.addEventListener(`input`, () => {
+    if (!regExEmail.test(theEmail.value)) {
+        theEmail.setCustomValidity(`Invalid email`);
+    } else {
+        theEmail.setCustomValidity(``);
+    }
+});
